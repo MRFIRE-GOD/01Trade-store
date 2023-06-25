5f041d3f18c24322021cc651c4786b6c4059c140
Thomas Tesfayohans <ttesfayohans22@gmail.com>
2023-06-25 22:26:09 +0200

    update

--- home.py
+++ home.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Welcome to the trading website!'
+    return 'Welcome to the trading website'
+
 if __name__ == '__main__':
     app.run(debug=True)
